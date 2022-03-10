a60d635bbfd6c35fe338b8c013e816053dc5553a
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-10 12:14:55 +0500

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "../../images/logo.png";
+
 const Navbar = () => {
   return (
     <div>Navbar</div>
