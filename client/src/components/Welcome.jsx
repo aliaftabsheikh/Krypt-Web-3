c4318d8a7ba366234c2991e10c72bfc22d87f8ee
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-10 12:45:30 +0500

    welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {AiFillPlayCircle} from "react-icons/ai"
+import {SiEthereum} from "react-icons/si"
+import {BiInfoCircle} from "react-icons/bs"
+
+import {Loader} from "./Loader"
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div>
+      
+    </div>
   )
 }
 
