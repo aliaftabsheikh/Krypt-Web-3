52a2fa48bd323a03da6fd42034eafaba6e9a680b
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-10 16:17:20 +0500

    welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import {AiFillPlayCircle} from "react-icons/ai"
-import {SiEthereum} from "react-icons/si"
-import {BiInfoCircle} from "react-icons/bs"
+import React from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
 
-import {Loader} from "./Loader"
+import { Loader } from "./";
 
 const Welcome = () => {
   return (
-    <div>
-      
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 p-12 px-4 ">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1>
+            Send Crypto <br/> across the world
+          </h1>
+          </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
