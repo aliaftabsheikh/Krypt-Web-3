5b046a53fd69ed7fff8fd6f2ae1f671a60aae853
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-10 12:11:44 +0500

    tailwind default styels

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-const App = ()=> {
+import React from "react"
+
+import { Navbar, Footer, Loader, Services, Transactions, Welcome } from "./components"
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  ) 
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  )
 }
 
-export default App
+export default App 
