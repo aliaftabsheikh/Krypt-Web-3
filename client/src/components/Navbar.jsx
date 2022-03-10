f85d72f30846eb4af020c02f9e7338e8a3ee1331
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-10 19:18:37 +0500

    welcome

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,21 +34,20 @@ const Navbar = () => {
           <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
         )}
         {toggleMenu && (
-          <motion.ul
-          whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeIn" }}
+          <ul
+          
             className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white"
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
             {["Market", "Exchange", "Tutorials", "Wallets"].map(
               (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
             )}
-          </motion.ul>
+          </ul>
         )}
       </div>
     </nav>
   );
-};
+};  
 
 export default Navbar;
