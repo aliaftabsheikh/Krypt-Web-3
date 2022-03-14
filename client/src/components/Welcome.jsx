a35d50d398600f1c2a16112b772b9f39ef9f5b6f
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-14 11:47:14 +0500

    solidity setup

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -47,9 +47,9 @@ const Welcome = () => {
           </button>
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
-            <div className={commonStyles}>Security</div>
-            <div className={`rounded-tr-2xl ${commonStyles}`}>Reliability</div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Binance</div>
+            <div className={commonStyles}>CoinMarketCap</div>
+            <div className={`rounded-tr-2xl${commonStyles}`}>Reliability</div>
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
             <div className={commonStyles}>Low fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
