e50ea66777886a81a89dcbe0b5162c20c9df6e84
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-14 16:27:25 +0500

    solidity connect wallet

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
+module.exports={
+  solidity : '0.8.0',
+  network : {
+    ropsten : {
+      url : 'https://eth-ropsten.alchemyapi.io/v2/0wVgEsn-O0izI2yPmJoM3HLfVPyqI0Qk',
+      accounts : ['d64ebc77ee0907b9ed534cf12b8f3535a5604b88538710decbf60394e6c852a9']
+    } 
   }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
-};
+}
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,16 +1,21 @@
 const main = async()=> {
 
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
 
-  await greeter.deployed();
+  await transactions.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Transactions deployed to:", transactions.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
+const runMain = async ()=>{
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
     process.exit(1);
-  });
+  }
+}
+
+runMain();
