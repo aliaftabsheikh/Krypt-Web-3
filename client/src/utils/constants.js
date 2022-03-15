0fb81ca8236d6a094439024124e168dc139ade41
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-15 12:32:03 +0500

    solidity

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x4283D275278748f3A804E2Ff69Eb16D3cB808627'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,7 +2,7 @@ require('@nomiclabs/hardhat-waffle');
 
 module.exports={
   solidity : '0.8.0',
-  network : {
+  networks : {
     ropsten : {
       url : 'https://eth-ropsten.alchemyapi.io/v2/0wVgEsn-O0izI2yPmJoM3HLfVPyqI0Qk',
       accounts : ['d64ebc77ee0907b9ed534cf12b8f3535a5604b88538710decbf60394e6c852a9']
