525624082526bc4b3775496551d7f9ae63fd26e9
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-16 16:27:10 +0500

    service section

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import { BsShieldFillCheck } from 'react-icons/bs';
-import {BiSearchAlt} from 'react-icons/bi';
-import {RiHeart2Fill} from 'react-icons/ri'
+import React from "react";
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
+
+const ServiceCard = ({ color, title, icon, subtitle }) => (
+  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl"></div>
+);
 
 const Services = () => {
   return (
-    <div className='flex w-full justify-center items-center gradient-bg-services'>
+    <div className="flex w-full justify-center items-center gradient-bg-services">
       <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
-          <h1 className='text-white text-3xl sm:text-5xl py-2 text-gradient'>
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
             Services that we
-            <br/>
+            <br />
             continue to improve
           </h1>
-          </div>
         </div>
+      </div>
+      <div className="flex-1 flex flex-col justify-start items-center">
+        <ServiceCard
+          color="bg-[#2952e3]"
+          title="Security Guranteed"
+          icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
