61091525364e3584c3c324be02c85b36a7894922
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-16 16:34:40 +0500

    service

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,9 +4,19 @@ import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
 const ServiceCard = ({ color, title, icon, subtitle }) => (
-  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl"></div>
+  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+    <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color} `}>
+      {icon}
+      </div>
+      <div className="ml-5 flex flex-col flex-1">
+      <h3 className="mt-2 text-white text-lg">{title}</h3>
+      <p className="mt-1 text-white text-sm md:w-9/12">
+        {subtitle}
+      </p>
+    </div>
+    </div>
 );
-
+ 
 const Services = () => {
   return (
     <div className="flex w-full justify-center items-center gradient-bg-services">
@@ -24,6 +34,18 @@ const Services = () => {
           color="bg-[#2952e3]"
           title="Security Guranteed"
           icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#8945F8]"
+          title="Best exchange rates"
+          icon={<BiSearchAlt fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#F84550]"
+          title="Fastest transactions"
+          icon={<RiHeart2Fill fontSize={21} className="text-white" />}
           subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
         />
       </div>
