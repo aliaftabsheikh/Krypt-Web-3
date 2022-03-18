b13b7b457ce781f63884f769787388a93f38228d
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-18 16:01:30 +0500

    tranasactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,32 +4,49 @@ import { TransactionsContext } from "../context/TransactionsContext";
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
-const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url})=>{
-    return (
-      <div className=""></div>
-)
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
+  return( 
+      <div className="bg-[#181918] m-4 flex flex-1
+      2xl:min-w-[450px]
+      2xl:max-w-[500px]
+      sm:min-w-[270px]
+      sm:max-w-[300px]
+      flex-col p-3 rounded-md hover:shadow-2xl
+      "></div>
+  )
+};
 
 const Transactions = () => {
-  const {currentAccount} = useContext(TransactionsContext)
-  return(
+  const { currentAccount } = useContext(TransactionsContext);
+  return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
-          <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
-        ):(
-          <h3 className="text-white text-3xl text-center my-2">Connect your account to see the Latest Transactions</h3>
-
+          <h3 className="text-white text-3xl text-center my-2">
+            Latest Transactions
+          </h3>
+        ) : (
+          <h3 className="text-white text-3xl text-center my-2">
+            Connect your account to see the Latest Transactions
+          </h3>
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i)=>(
-            <TransactionCard key={i} {...transaction}/>
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
           ))}
-          </div>
-        
         </div>
       </div>
+    </div>
   );
-}
+};
 
 export default Transactions;
