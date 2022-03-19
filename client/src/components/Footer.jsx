e91b8b075196c81c8bf7c3b1ce14624c4eb067c9
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-19 12:19:41 +0500

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import logo from '../images/logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer'>
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className='flex flex-[0.5] justify-center items-center'>
+          <img src={logo} alt="logo" className='w-32' />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full ">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
